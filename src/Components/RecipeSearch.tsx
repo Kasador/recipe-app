c3e435188bc4f9520ed31fc08d7203a3c42c41d6
Kasador <huntershaw23@gmail.com>
2019-07-02 13:36:42 -0500

    FIRST: setup workflow with SASS and TypeScript.

---
+++ src/Components/RecipeSearch.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecipeSearch = () => {
+    return (
+        <div className="RecipeSearch">
+            <p>test</p>
+        </div> 
+    );
+}
+
+export default RecipeSearch;
---
+++ src/Main/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RecipeSearch from '../Components/RecipeSearch';
+
+function App() {
+    return (
+        <div className="App">
+            <RecipeSearch />
+        </div>
+    );
+}
+
+export default App;
