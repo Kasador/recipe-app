b7ce568af88f9b7d85aebe48b3cbf5074fe0826e
Kasador <huntershaw23@gmail.com>
2019-07-03 16:47:33 -0500

    ADDED: working searchbox that returns results plus some error handling.

--- src/Components/RecipeSearch.js
+++ src/Components/RecipeSearch.js
@@ -2,27 +2,59 @@ import React, { useState }from 'react';
 import Axios from 'axios';
 
 const RecipeSearch = () => {
-    // const [user, setUser] = useState({
-    //     persons: [],
-    //     isLoaded: false
-    // });
-    
-    // const Search = () => {
-    //     Axios.get('https://api.edamam.com/search?q=chicken&app_id=4ed96582&app_key=62c3a0bc3f607c7c08a09fbe51007e87&from=0&to=3&calories=591-722&health=alcohol-free')
-    //     .then(res => {
-    //         console.log(res.data);
-    //         setUser({
-    //             persons: res.data,
-    //             isLoaded: true
-    //         })
-    //     });
-    // }
+    // API data and info
+    const AppId = '4ed96582',
+        AppKey = '62c3a0bc3f607c7c08a09fbe51007e87';
+
+    let searchItem;
+
+    // useState hook for input search value and store get response
+    const [search, setSearch] = useState({
+        value: '',
+        recipes: [],
+        isLoaded: false
+    })
+
+    // search data
+    const handlerUpdateSearchValue = (e) => {
+        setSearch({...search, value: e.target.value})
+        console.log(search);
+    }
+
+    // on click search button, query API and get response
+    const handleSearchRequest = () => {
+        if (search.value === '') {
+            alert('The search box is empty!');
+        } else {
+            searchItem = search.value;
+            console.log(searchItem);
+
+            Axios.get('https://api.edamam.com/search?q=' + searchItem + '&app_id=' + AppId + '&app_key=' + AppKey + '&from=0&to=100')
+            .then(res => {
+                console.log(res.data);
+                console.log(res.data.hits.length);
+
+                if (res.data.hits.length === 0) {
+                    alert('No results found...');
+                } else if (res.data.hits.length !== 0) {
+                    alert('Found ' + res.data.hits.length + ' results out of ' + res.data.count + ' total recipes.');
+
+                    setSearch({
+                        value: '',
+                        recipes: res.data,
+                        isLoaded: true
+                    })
+                }
+            });
+        }
+    }
 
     return (
         <div className="RecipeSearch">
-            <div className="search">
+            <div className="Search">
                 <span className="fa fa-search"></span>
-                <input placeholder="Search recipes..." />
+                <input placeholder="Search recipes..." value={search.value} onChange={handlerUpdateSearchValue} />
+                <button className="SearchBtn" onClick={handleSearchRequest}>Search</button>
             </div>
         </div> 
     );
--- src/Components/ResultSearch.js
+++ src/Components/ResultSearch.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ResultSearch = () => {
     return (
         <div className="ResultSearch">
-            <h1>test</h1>
+            
         </div>
     );
 }
