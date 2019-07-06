db96fa4d1871ed80ced1e5f19d690b589ee89143
Kasador <huntershaw23@gmail.com>
2019-07-06 17:35:41 -0500

    FIXED: makes new card for each new set of info.

--- src/Components/RecipeCard.js
+++ src/Components/RecipeCard.js
@@ -43,31 +43,32 @@ const RecipeCard = (props) => {
     return (
         <div className="RecipeCard">
             {data.isLoaded ?
-                <div className="RecipeCardWrapper">
-                    <div className="RecipeTopHalf">
-                        <img src={data.recipes.hits[0].recipe.image} alt="test" className="RecipeImages" />
-                        <div className="RecipeMainInfo">
-                            <h1 className="RecipeTitle">{data.recipes.hits[0].recipe.label}</h1>
-                            <div className="RecipeInfoBasic">
-                                <span className="RecipeTime">Prep Time: 2:00</span>
-                                <span className="RecipeCalories">Total Cal: 566</span>
+            <div>
+                {data.recipes.hits.map((hit) => {
+                    return (
+                    <div className="RecipeCardWrapper">
+                        <div className="RecipeTopHalf">
+                            <img src={data.recipes.hits[0].recipe.image} alt="test" className="RecipeImages" />
+                            <div className="RecipeMainInfo">
+                                <h1 className="RecipeTitle">{hit.recipe.label}</h1>
+                                <div className="RecipeInfoBasic">
+                                    <span className="RecipeTime">Prep Time: 2:00</span>
+                                    <span className="RecipeCalories">Total Cal: 566</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="RecipeBottomHalf">
-                        <div className="RecipeIngredients">
-                            <ul>
-                                <li>ingredient name</li>
-                                <li>ingredient name</li>
-                                <li>ingredient name</li>
-                            </ul>
+                        <div className="RecipeBottomHalf">
+                            <div className="RecipeIngredients">
+                                <ul>
+                                    <li>ingredient name</li>
+                                    <li>ingredient name</li>
+                                    <li>ingredient name</li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            : null}
-            {/* {data.recipes.map((recipe) => {
-                return <li>{recipe}</li>
-            })} */}
+                    </div>);
+                })}
+            </div>: null}
         </div>
     );
 }
